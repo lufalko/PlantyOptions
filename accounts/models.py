8b465faafb3927333bd309e182b48f1c16f1f206
robing <robin.geramb@hs-augsburg.de>
2020-11-13 17:38:58 +0100

    restaurants webpage erweiterungen

--- accounts/models.py
+++ accounts/models.py
@@ -27,7 +27,6 @@ class Tag(models.Model):
 class Restaurant(models.Model):
     name = models.CharField(max_length=200, null=True)
     location = models.CharField(max_length=200, null=True)
-    image = models.ImageField(null=True, blank=True)
     rating = models.FloatField(null=True)
     tags = models.ManyToManyField(Tag)
     affordability = models.FloatField(null=True)
