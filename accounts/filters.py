8117da3c8406f9f16fa12c8ad25b23452f40c8f1
lucasfalkowsky <lucas.falkowsky@hs-augsburg.de>
2020-11-13 21:00:10 +0100

    search Filter for restaurants

--- accounts/filters.py
+++ accounts/filters.py
@@ -1,9 +1,12 @@
 import django_filters
+from django_filters import DateFilter
 
 from .models import *
 
 
 class RestaurantFilter(django_filters.FilterSet):
+
     class Meta:
         model = Restaurant
         fields = '__all__'
+        exclude = ['location']
