12172e7f23b15cd28445a13958be54546f6cfb14
lucasfalkowsky <lucas.falkowsky@hs-augsburg.de>
2020-11-22 17:54:14 +0100

    greater than/equal to rating filter

--- accounts/filters.py
+++ accounts/filters.py
@@ -1,12 +1,13 @@
 import django_filters
-from django_filters import DateFilter
+from django_filters import NumberFilter
 
 from .models import *
 
 
 class RestaurantFilter(django_filters.FilterSet):
 
+    rating = NumberFilter(field_name="rating", lookup_expr="gte")
     class Meta:
         model = Restaurant
         fields = '__all__'
-        exclude = ['location', 'restaurant_picture']
+        exclude = ['location', 'restaurant_picture', 'rating']
